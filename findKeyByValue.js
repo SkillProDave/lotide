bf461fb8aa0fd9723975d348adb34d7f9deca8b1
Dave Curry <Dave@SkillPro.com.com>
2021-04-01 00:18:04 +0000

    terrible intro to Objects ;-(

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+/////////////////
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+console.log(bestTVShowsByGenre);
+
+const findKeyByValue  = (objectName,showName) => {
+  for (const objKey in objectName) {
+    if (objectName[objKey] === showName) {
+      return objKey;
+    }
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
